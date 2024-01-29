50e38fa9facfe91f9c93a21161bd2f960469a8b2
Venislav84 <133969005+Venislav84@users.noreply.github.com>
2024-01-29 21:13:30 +0200

    Add files via upload

---
+++ EvenOrOdd/EvenOrOdd/EvenOrOdd.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void evenOrOdd() {
+    int num;
+    cout << "Enter a sequence of positive integers numbers " << endl;
+    while (cin >> num){
+        if (num > 0) {
+            if (num % 2 == 0) {
+                cout << "Number is even: " << num << endl;
+            }
+            else {
+                cout << "Number is odd: " << num << endl;
+            }
+        }
+        else {
+            cout << "Please enter only a positive numbers" << endl;
+        }
+        
+    }
+}
+
+int main()
+{
+    evenOrOdd();
+
+    return 0;
+}
+
+
