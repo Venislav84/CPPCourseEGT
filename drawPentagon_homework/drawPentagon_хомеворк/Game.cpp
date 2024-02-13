229d110c69fee922cc4fc4fe7f5ea986dd89d0c1
Venislav84 <133969005+Venislav84@users.noreply.github.com>
2024-02-13 17:13:44 +0200

    homework

---
+++ drawPentagon_homework/drawPentagon_хомеворк/Game.cpp
@@ -0,0 +1,103 @@
+#include "Game.h"
+
+using namespace std;
+
+bool Game::init(const char* title, int xpos, int ypos, int width, int height, int flags)
+{
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
+        cout << "SDL init success\n";
+
+        window = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
+        if (window != 0) {
+            cout << "window creation success\n";
+            renderer = SDL_CreateRenderer(window, -1, 0);
+            if (renderer != 0) {
+                cout << "renderer creation success\n";
+
+
+            }
+            else {
+                cout << "renderer init failed\n";
+                return false;
+            }
+        }
+        else {
+            cout << "window init failed\n";
+            return false;
+        }
+    }
+    else {
+        cout << "SDL init fail\n";
+        return false;
+    }
+    cout << "init success\n";
+    running = true;
+    return true;
+
+}
+
+void Game::render()
+{
+    SDL_SetRenderDrawColor(renderer, 120, 250, 37, 99);
+
+    SDL_RenderClear(renderer);
+
+
+    int ww, wh;
+    SDL_GetWindowSize(window, &ww, &wh);
+ 
+    SDL_SetRenderDrawColor(renderer, 0, 255, 255, 50);
+
+    Shapes::drawPentagon(renderer, ww / 2, wh / 2, 200);
+    Shapes::drawPentagon(renderer, ww / 2, wh / 2, 201);
+    Shapes::drawPentagon(renderer, ww / 2, wh / 2, 202);
+    Shapes::drawPentagon(renderer, ww / 2, wh / 2, 199);
+    Shapes::drawPentagon(renderer, ww / 2, wh / 2, 198);
+
+    SDL_RenderPresent(renderer);
+}
+
+void Game::update()
+{
+
+}
+
+void Game::handleEvents()
+{
+    SDL_Event event;
+    if (SDL_PollEvent(&event)) {
+        switch (event.type) {
+        case SDL_QUIT:
+            running = false;
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+void Game::clean()
+{
+    cout << "cleaning game\n";
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    SDL_Quit();
+}
+
+bool Game::isRunning()
+{
+    return this->running;
+}
+Game::Game()
+{
+    Game::window = NULL;
+    Game::renderer = NULL;
+    Game::running = true;
+    Game::currentFrame = 0;
+ 
+}
+
+Game::~Game()
+{
+}
---
+++ drawPentagon_homework/drawPentagon_хомеворк/Shapes.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Game.h"
+
+class Shapes {
+
+public:
+
+	static void drawPentagon(SDL_Renderer* renderer, int centerX, int centerY, int side);
+
+};
+
